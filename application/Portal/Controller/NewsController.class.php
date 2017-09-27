8de70d00576403cc2d3b3f75d15efc2e32dd52f9
wangchong <894377341@qq.com>
2017-09-27 18:17:59 +0800

    cms

---
+++ application/Portal/Controller/NewsController.class.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ *      _______ _     _       _     _____ __  __ ______
+ *     |__   __| |   (_)     | |   / ____|  \/  |  ____|
+ *        | |  | |__  _ _ __ | | _| |    | \  / | |__
+ *        | |  | '_ \| | '_ \| |/ / |    | |\/| |  __|
+ *        | |  | | | | | | | |   <| |____| |  | | |
+ *        |_|  |_| |_|_|_| |_|_|\_\\_____|_|  |_|_|
+ */
+/*
+ *     _________  ___  ___  ___  ________   ___  __    ________  _____ ______   ________
+ *    |\___   ___\\  \|\  \|\  \|\   ___  \|\  \|\  \ |\   ____\|\   _ \  _   \|\  _____\
+ *    \|___ \  \_\ \  \\\  \ \  \ \  \\ \  \ \  \/  /|\ \  \___|\ \  \\\__\ \  \ \  \__/
+ *         \ \  \ \ \   __  \ \  \ \  \\ \  \ \   ___  \ \  \    \ \  \\|__| \  \ \   __\
+ *          \ \  \ \ \  \ \  \ \  \ \  \\ \  \ \  \\ \  \ \  \____\ \  \    \ \  \ \  \_|
+ *           \ \__\ \ \__\ \__\ \__\ \__\\ \__\ \__\\ \__\ \_______\ \__\    \ \__\ \__\
+ *            \|__|  \|__|\|__|\|__|\|__| \|__|\|__| \|__|\|_______|\|__|     \|__|\|__|
+ */
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2014 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: Dean <zxxjjforever@163.com>
+// +----------------------------------------------------------------------
+namespace Portal\Controller;
+use Common\Controller\HomebaseController; 
+/**
+ * 新闻热点
+ */
+class NewsController extends HomebaseController {
+	
+    //新闻热点
+	public function index() {
+		$d = M("term_relationships");
+		$count = $d->where('term_id = 25')->count('object_id');
+		$Page       = new \Think\Page($count,8);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$show       = $Page->show();// 分页显示输出
+		// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+		$list = $d->join('cmf_posts on cmf_term_relationships.object_id = cmf_posts.id')->
+		where('term_id = 25')->limit($Page->firstRow . ',' . $Page->listRows)->select();
+		$this->assign('list',$list);// 赋值数据集
+		$this->assign('page',$show);// 赋值分页输出
+    	$this->display(":news");
+    }
+    public function newInfo(){
+    	$d = M('posts');
+    	$new = $d->find(I("get.id"));
+    	$this->assign('new',$new);
+    	$this->assign("new_id",$new['id']);
+    	$this->assign("new_title",$new['post_title']);
+    	$this->display(":newsInfo");
+    }
+
+}
+
+
